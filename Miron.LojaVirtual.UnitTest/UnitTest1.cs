c9a2d960732ffaf2efe5584b81279dbfb20ef7d3
xandesbarbosa <xandesbarbosa76@outlook.com>
2016-03-04 22:02:59 -0300

    Initial Commit

---
+++ Miron.LojaVirtual.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Miron.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class UnitMiron
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Miron.LojaVirtual.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Miron.LojaVirtual.Web.MvcApplication" Language="C#" %>

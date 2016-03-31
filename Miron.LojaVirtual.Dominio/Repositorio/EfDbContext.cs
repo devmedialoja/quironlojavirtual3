701d7f8a6ddf541a755cad33fba7c0ff0445f800
xandesbarbosa <xandesbarbosa76@outlook.com>
2016-03-30 21:20:07 -0300

    Entity

---
+++ Miron.LojaVirtual.Dominio/Repositorio/EfDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Miron.LojaVirtual.Dominio.Entidade;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miron.LojaVirtual.Dominio.Repositorio
+{
+    public class EfDbContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Entity<Produto>().ToTable("Produtos");
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
